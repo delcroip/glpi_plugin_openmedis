bbeefd2105e0302b7a44ea63e59700a31f63d047
Patrick Delcroix <pmpdelcroix@gmail.com>
2021-06-27 16:31:45 +0200

    new licence

--- install/install.class.php
+++ install/install.class.php
@@ -1,33 +1,33 @@
 <?php
 /**
- * LICENSE
- *
- * Copyright © 2016-2018 Teclib'
- * Copyright © 2010-2018 by the FusionInventory Development Team.
- * Copyright © 2021 by Patrick delcroix <patrick@pmpd.eu>
- * This file is part of openmedis Plugin for GLPI.
- *
- * Flyve MDM Plugin for GLPI is a subproject of Flyve MDM. Flyve MDM is a mobile
- * device management software.
- *
- * Flyve MDM Plugin for GLPI is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * Flyve MDM Plugin for GLPI is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License
- * along with Flyve MDM Plugin for GLPI. If not, see http://www.gnu.org/licenses/.
- * ------------------------------------------------------------------------------
- * @copyright Copyright © 2018 Teclib
- * @license   https://www.gnu.org/licenses/agpl.txt AGPLv3+
- * @link      https://github.com/flyve-mdm/glpi-plugin
- * @link      https://flyve-mdm.com/
- * ------------------------------------------------------------------------------
- */
+ -------------------------------------------------------------------------
+  LICENSE
 
+ This file is part of openMEDIS plugin for GLPI.
+
+ Reports is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ openMEDIS is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with Reports. If not, see <http://www.gnu.org/licenses/>.
+
+ @package   openmedis
+ @authors   Patrick Delcroix
+ @copyright Copyright (c) 2009-2021 Reports plugin team
+ @license   AGPL License 3.0 or (at your option) any later version
+            http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ @link      https://github.com/delcroip/glpi_open_medis
+ @link      http://www.glpi-project.org/
+ @since     2021
+ --------------------------------------------------------------------------
+ */
 if (!defined('GLPI_ROOT')) {
    die("Sorry. You can't access this file directly");
 }
--- install/upgrade_to_1_2.php
+++ install/upgrade_to_1_2.php
@@ -1,32 +1,32 @@
 <?php
 /**
- * LICENSE
- *
- * Copyright © 2016-2018 Teclib'
- * Copyright © 2010-2018 by the FusionInventory Development Team.
- * Copyright © 2010-2018 by patrick Delcroix <patrick@pmpd.eu>
- *
- * This file is part ofopenMedis Plugin for GLPI.
- *
- *openMedis Plugin for GLPI is a subproject ofopenMedis.openMedis is a mobile
- * device management software.
- *
- *openMedis Plugin for GLPI is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *openMedis Plugin for GLPI is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License
- * along with openMedis Plugin for GLPI. If not, see http://www.gnu.org/licenses/.
- * ------------------------------------------------------------------------------
- * @copyright Copyright © 2018 Teclib
- * @license   https://www.gnu.org/licenses/agpl.txt AGPLv3+
- * @link      https://github.com/delcroip/glpi_open_medis
+ -------------------------------------------------------------------------
+  LICENSE
 
- * ------------------------------------------------------------------------------
+ This file is part of openMEDIS plugin for GLPI.
+
+ Reports is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ openMEDIS is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with Reports. If not, see <http://www.gnu.org/licenses/>.
+
+ @package   openmedis
+ @authors   Patrick Delcroix
+ @copyright Copyright (c) 2009-2021 Reports plugin team
+ @license   AGPL License 3.0 or (at your option) any later version
+            http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ @link      https://github.com/delcroip/glpi_open_medis
+ @link      http://www.glpi-project.org/
+ @since     2021
+ --------------------------------------------------------------------------
  */
 
 
